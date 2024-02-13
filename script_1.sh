74f88368d61aa81a6e18f215d4359205c8b4bded
jackr276 <jack_m_robbins@me.com>
2024-02-13 13:07:28 -0500

    Update script_1.sh

--- script_1.sh
+++ script_1.sh
@@ -4,7 +4,34 @@
 #!bin/bash
 
 #Use grep for regular expression matching to get all of the dates
-dates=( $(grep -o '[0-9][0-9][\./-][0-9][0-9][\./-][0-2]0[0-2][0-9]' "$1") )
+dates=( $(grep -o '[0-9][0-9][\./-][0-9][0-9][\./-][0-9][0-9][0-9][0-9]' "$1") )
+size=${#dates[@]}
 
-echo ${dates[@]}
+declare -A unixTime
+declare -a timeOnly=()
+
+#translate everything into unix time(seconds since 1970)
+for((i = 0; i < $size; i++)); do
+    #grab the date out of the array for convenience
+    date=${dates[i]}
+    #grab year
+    [[ $date =~ ([0-9]{4}) ]]
+    year=${BASH_REMATCH[1]}
+    #grab month
+    [[ $date =~ ([0-9]{2}) ]]
+    month=${BASH_REMATCH[1]}
+    #grab day
+    [[ $date =~ ([\./-][0-9]{2}) ]]
+    day=$(echo ${BASH_REMATCH[1]} | cut -c 2-3)
+    #calculate the time since 1970 using date
+    timeSeconds=$(date +%s --date "${year}-${month}-${day}")
+    #store the time since 1970 as a key in the associative array
+    unixTime["$timeSeconds"]="$date"
+    timeOnly+=("$timeSeconds")
+done
+
+echo "${!unixTime[@]}"
+
+
+
 
