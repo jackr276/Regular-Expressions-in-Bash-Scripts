3c17c46757c0f8d20d17f63a8840ab0763b0b6c3
jackr276 <jack_m_robbins@me.com>
2024-02-13 13:21:05 -0500

    script_1 working as expected

--- script_1.sh
+++ script_1.sh
@@ -27,11 +27,33 @@ for((i = 0; i < $size; i++)); do
     timeSeconds=$(date +%s --date "${year}-${month}-${day}")
     #store the time since 1970 as a key in the associative array
     unixTime["$timeSeconds"]="$date"
+    #also store the unix time for ease of sorting
     timeOnly+=("$timeSeconds")
 done
 
-echo "${!unixTime[@]}"
+#Now we need to sort the timeOnly array(using insertion sort algorithm)
+#get size of array
+size=${#timeOnly[@]}
+#insertion sort algorithm
+for((i = 1; i < $size; i++)); do
+    #get each array element as a key
+    key=(${timeOnly[i]})
 
+    #Declare j
+    j=$(($i-1))
 
+    #while j more than key, perform swap
+    while [[ $j -ge 0 ]] && [[ ${timeOnly[j]} -gt $key ]]; do
+            #bring array[j] forward
+            timeOnly[$(($j+1))]=${timeOnly[j]}
+            #decrement j with arithmetic expansion
+            ((j--))
+        done
+        #put key in the next spot
+        timeOnly[$(($j+1))]=$key
+done
 
-
+#Finally, print out everything in order
+for entry in "${timeOnly[@]}"; do
+    echo ${unixTime["$entry"]}
+done
