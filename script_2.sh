dfeb44eacae379f5df16e42aae8c266d28c83679
jackr276 <jack_m_robbins@me.com>
2024-02-15 21:06:25 -0500

    age calculation now working

--- script_2.sh
+++ script_2.sh
@@ -7,6 +7,8 @@
 
 #Declare an associative array for holding people and their ages
 declare -A peopleAge
+#Get the current time in seconds for calculating age
+timeSeconds=$(date +%s)
 
 #Read the file in line by line, checking for people who have a two word city
 while read -r line; do
@@ -17,7 +19,12 @@ while read -r line; do
 	echo "$name"
 	#We expect to see dates in YYYY-MM-DD
         dob=$(echo "$line" | grep -E -o '[0-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]')
-	echo "$dob"
+	dobSeconds=$(date +%s --date "$dob")
+	ageSeconds=$(($timeSeconds-$dobSeconds))
+	#Note: 1 year = 31557600 seconds
+	ageInYears=$((ageSeconds/31557600)) 
+	echo "$ageInYears"
+
     fi
 
 done < "$1"
