43993f8e06ca51d609e98597cea70b2dada33a72
jackr276 <jack_m_robbins@me.com>
2024-02-15 21:12:28 -0500

    age calculation now working

--- script_2.sh
+++ script_2.sh
@@ -7,6 +7,9 @@
 
 #Declare an associative array for holding people and their ages
 declare -A peopleAge
+#Declare a regular array for holding people
+declare -a people=()
+
 #Get the current time in seconds for calculating age
 timeSeconds=$(date +%s)
 
@@ -16,15 +19,20 @@ while read -r line; do
     if [[ $(echo "$line" | grep -E ',([[:space:]][A-Za-z]{1,}){2,},') ]]; then
 	#We need to grab the person's name and DOB(to calculate age) if here
         name=$(echo "$line" | grep -E -o '^[A-Za-z]{1,}[[:space:]][A-Za-z]{1,}' )
-	echo "$name"
+	people+=("$name")
+
 	#We expect to see dates in YYYY-MM-DD
         dob=$(echo "$line" | grep -E -o '[0-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]')
+
+	#Convert age into seconds since 1970 for calculations, then convert the difference into years
 	dobSeconds=$(date +%s --date "$dob")
 	ageSeconds=$(($timeSeconds-$dobSeconds))
+
+	#convert to age in years
 	#Note: 1 year = 31557600 seconds
-	ageInYears=$((ageSeconds/31557600)) 
-	echo "$ageInYears"
-
+	ageInYears=$((ageSeconds/31557600))
+	peopleAge["$name"]="$ageInYears"
     fi
-
 done < "$1"
+
+echo "${peopleAge[@]}"
